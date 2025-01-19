8b0de4a05cdd89600d1ad0a8837f3bfd049a7f23
anatorini <marcin.szymczak156@gmail.com>
2025-01-19 22:44:56 +0100

    2024-day4-zig-part1

---
+++ 2024/zig/day4/src/main.zig
@@ -0,0 +1,115 @@
+const std = @import("std");
+
+const pos = struct { x: usize, y: usize };
+
+const horizontal = enum { left, right, ignore };
+const vertical = enum { up, down, ignore };
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const alloc = gpa.allocator();
+    var args = std.process.args();
+    _ = args.skip();
+    const filename = args.next() orelse {
+        std.debug.print("Filename missing", .{});
+        return;
+    };
+    const file = std.fs.cwd().openFile(filename, .{}) catch {
+        std.debug.print("Failed to open a file", .{});
+        return;
+    };
+    defer file.close();
+
+    var buffer = std.ArrayList(u8).init(alloc);
+    const writer = buffer.writer();
+    const reader = file.reader();
+    var data: [150][]u8 = undefined;
+    var line: usize = 0;
+    var rval: u32 = 0;
+    while (true) {
+        reader.streamUntilDelimiter(writer, '\n', null) catch |e| switch (e) {
+            error.EndOfStream => break,
+            error.StreamTooLong => {
+                std.debug.print("Line to long", .{});
+                return;
+            },
+            else => unreachable,
+        };
+        if (buffer.items.len < 3) continue;
+        data[line] = try alloc.alloc(u8, buffer.items.len);
+        std.mem.copyForwards(u8, data[line], buffer.items);
+        line += 1;
+        buffer.clearRetainingCapacity();
+    }
+
+    for (0..line) |row| {
+        for (0..data[row].len) |col| {
+            if (data[row][col] == 'X') {
+                rval += explore_around(data[0..line], .{ .x = col, .y = row });
+            }
+        }
+    }
+    std.debug.print("Part 1: {}\n", .{rval});
+
+    for (0..line) |row| {
+        alloc.free(data[row]);
+    }
+    buffer.deinit();
+    _ = gpa.deinit();
+}
+
+const pattern = [_]u8{ 'X', 'M', 'A', 'S' };
+
+fn explore_direction(data: [][]u8, start: pos, h: horizontal, v: vertical) bool {
+    var col: usize = undefined;
+    var row: usize = undefined;
+    for (1..4) |i| {
+        col = switch (h) {
+            .left => start.x - i,
+            .right => start.x + i,
+            .ignore => start.x,
+        };
+        row = switch (v) {
+            .up => start.y - i,
+            .down => start.y + i,
+            .ignore => start.y,
+        };
+        if (data[row][col] != pattern[i]) return false;
+    }
+    return true;
+}
+
+fn explore_around(data: [][]u8, start: pos) u32 {
+    var rval: u32 = 0;
+    if (start.x >= 3) {
+        if (start.y >= 3) {
+            rval += if (explore_direction(data, start, .left, .up)) 1 else 0;
+        }
+        if (start.y + 3 < data.len) {
+            rval += if (explore_direction(data, start, .left, .down)) 1 else 0;
+        }
+        rval += if (explore_direction(data, start, .left, .ignore)) 1 else 0;
+    }
+    if (start.x + 3 < data[0].len) {
+        if (start.y >= 3) {
+            rval += if (explore_direction(data, start, .right, .up)) 1 else 0;
+        }
+        if (start.y + 3 < data.len) {
+            rval += if (explore_direction(data, start, .right, .down)) 1 else 0;
+        }
+        rval += if (explore_direction(data, start, .right, .ignore)) 1 else 0;
+    }
+    if (start.y >= 3) {
+        rval += if (explore_direction(data, start, .ignore, .up)) 1 else 0;
+    }
+    if (start.y + 3 < data.len) {
+        rval += if (explore_direction(data, start, .ignore, .down)) 1 else 0;
+    }
+    return rval;
+}
+
+test "test" {
+    for (1..3) |i| {
+        std.debug.print("{}", .{i});
+    }
+}
